8691f082a4bc28cd1a43d3186643347e4eb340c8
Peem040 <peem1016@gmail.com>
2024-06-11 18:45:37 +0700

    Add files via upload

---
+++ Exercise5_ 1_Suppakorn_S.py
@@ -0,0 +1,7 @@
+x = float(input("Fist Number:"))
+y = float(input("Second Number:"))
+
+print(x,'+',y,'=',x+y)
+print(x,'-',y,'=',x-y)
+print(x,'*',y,'=',x*y)
+print(x,'/',y,'=',x/y)
---
+++ Exercise5_2_Suppakorn_S.py
@@ -0,0 +1,4 @@
+x = float(input("Distant(m):"))
+y = float(input("Time(s):"))
+z = x/y
+print("Speed:", z ,"m/s")
