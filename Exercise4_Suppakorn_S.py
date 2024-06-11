1f8755ecada862259b3c6568c0bb1af2c8ef9556
Peem040 <peem1016@gmail.com>
2024-06-11 16:43:11 +0700

    Add files via upload

---
+++ Exercise4_Suppakorn_S.py
@@ -0,0 +1,9 @@
+FE = 36.5
+GB = 45
+ICS = 27.8
+CP = 67
+
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems :", ICS)
+print("Computer Programming :", CP)
