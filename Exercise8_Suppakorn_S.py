b158ed708d2f9cdd1717890dcaaea3d14257db66
Peem040 <peem1016@gmail.com>
2024-06-12 23:52:00 +0700

    Add files via upload

---
+++ Exercise8_Suppakorn_S.py
@@ -0,0 +1,36 @@
+username = input("Username : ")
+password = input("Password : ")
+
+if username == 'peem' and password == '1234':
+    print("    Welcome to Peem's Shop")
+    print("------------------------------")
+    print("You can select the product to purchase.")
+    print("1. Pencil        10 THB")
+    print("2. Pen           20 THB")
+    print("3. Pencil case   100 THB")
+    print("4. Ruler         20 THB")
+    print("5. Scissors      40 THB")
+    print("------------------------------")
+    choose = int(input("Choose : "))
+    if choose == 1:
+        x = input("Fill number of product : ")
+        print("Total : 10 x", x, '=', 10 * int(x), "THB")
+        print("------------------------------")
+    if choose == 2:
+        x = input("Fill number of product : ")
+        print("Total : 20 x", x, '=', 20 * int(x), "THB")
+        print("------------------------------")
+    if choose == 3:
+        x = input("Fill number of product : ")
+        print("Total : 100 x", x, '=', 100 * int(x), "THB")
+        print("------------------------------")
+    if choose == 4:
+        x = input("Fill number of product : ")
+        print("Total : 20 x", x, '=', 20 * int(x), "THB")
+        print("------------------------------")
+    if choose == 5:
+        x = input("Fill number of product : ")
+        print("Total : 40 x", x, '=', 40 * int(x), "THB")
+        print("------------------------------")
+else:
+    print("Incorrect")
